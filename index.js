462cb79194dbb1bcee11c33aa4d349fabc57c722
JPeterson1125 <119976248+JPeterson1125@users.noreply.github.com>
2023-04-24 16:12:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🏆 Player 1 Wins! 🏆"
+  document.querySelector("h1").innerHTML = "🏆 Player 1 Wins! 🏆";
 } else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "🏆 Player 2 Wins! 🏆"
+  document.querySelector("h1").innerHTML = "🏆 Player 2 Wins! 🏆";
+} else {
+  document.querySelector("h1").innerHTML = "IT'S A DRAW";
 }
-else {
-  document.querySelector("h1").innerHTML = "IT'S A DRAW"
-}
+
